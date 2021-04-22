0232f407d154da84b986c2af2dfdc899c42fdad0
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2021-04-22 20:16:16 +0800

    not finished

--- src/core/calculate/octree/utils.rs
+++ src/core/calculate/octree/utils.rs
@@ -2,19 +2,21 @@ use crate::core::calculate::octree::point::Point;
 use crate::core::calculate::octree::Octree;
 
 impl Octree {
-    fn walk(node: &Octree, f: &fn(octree: &Point)) {
+    fn get_all_points(node: &Octree, vec: &mut Vec<Point>) {
         if node.point.is_some() {
-            f(&node.point.unwrap());
+            vec.push(node.point.unwrap());
         }
         return (&node.children).into_iter().for_each(|c| {
             if c.is_some() {
-                Octree::walk(&c.as_ref().unwrap(), f);
+                return Octree::get_all_points(&c.as_ref().unwrap(), vec);
             }
         });
     }
 
-    pub fn for_each(&self, f: fn(octree: &Point)) {
-        Octree::walk(&self, &f);
+    pub fn write(&self, path: &str) {
+        let mut points: Vec<Point> = vec![];
+        Octree::get_all_points(&self, &mut points);
+        println!("{:?}", points);
     }
 }
 
@@ -23,7 +25,20 @@ mod tests {
     use super::*;
     use crate::core::calculate::octree::point::Point;
     use crate::core::calculate::octree::vec3::Vec3;
+    use crate::utils::msg_const::PROJ_DIR;
     use rand::{thread_rng, Rng};
+
+    #[test]
+    fn test_get_all_points() {
+        // let mut tree = Octree::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(50.0, 50.0, 50.0));
+        // for index in 0..100000 {
+        //     let point = Point::new(Vec3::new(1.0, 1.0, 1.0), 1);
+        //     tree.insert(point);
+        // }
+        // let mut points: Vec<Point> = vec![];
+        // Octree::get_all_points(&tree, &mut points);
+        // assert_eq!(points.len(), 100000);
+    }
 
     #[test]
     fn test_octree() {
@@ -31,7 +46,7 @@ mod tests {
         let mut tree = Octree::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(50.0, 50.0, 50.0));
 
         let mut rng = thread_rng();
-        for index in 0..2 {
+        for index in 0..100000 {
             let x = rng.gen_range(-50.0..50.0);
             let y = rng.gen_range(-50.0..50.0);
             let z = rng.gen_range(-50.0..50.0);
@@ -39,6 +54,6 @@ mod tests {
             tree.insert(point);
         }
 
-        tree.for_each(|c| println!("{:?}", c));
+        tree.write(&format!("{}/target/test_octree.octree", PROJ_DIR));
     }
 }
